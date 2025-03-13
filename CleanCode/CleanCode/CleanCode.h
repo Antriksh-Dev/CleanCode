984f0fe6bc23c5a3e7ee7b70c648f36f7a1d05e2
Antriksh Verma <connecttoantriksh@gmail.com>
2025-03-13 15:13:16 +0530

    Create CleanCode macOS fremework

---
+++ CleanCode/CleanCode/CleanCode.h
@@ -0,0 +1,18 @@
+//
+//  CleanCode.h
+//  CleanCode
+//
+//  Created by Antriksh Verma on 3/13/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanCode.
+FOUNDATION_EXPORT double CleanCodeVersionNumber;
+
+//! Project version string for CleanCode.
+FOUNDATION_EXPORT const unsigned char CleanCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanCode/PublicHeader.h>
+
+
