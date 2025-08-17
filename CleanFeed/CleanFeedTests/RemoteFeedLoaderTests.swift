96d360911e2da70f6ac132e90ebc7cb4280b7cf6
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-18 01:04:15 +0530

    Add 'Load Feed From Remote Use Case'

--- CleanFeed/CleanFeedTests/RemoteFeedLoaderTests.swift
+++ CleanFeed/CleanFeedTests/RemoteFeedLoaderTests.swift
@@ -5,6 +5,27 @@
 //  Created by Antriksh Verma on 8/18/25.
 //
 
+/*
+ 
+ Load Feed From Remote Use Case
+ 
+ Data: URL
+ 
+ Primary course (happy path):
+ 1. System execute "Load Feed" command with above data
+ 2. System downloads data from the URL
+ 3. System validates the downloaded data
+ 4. System creates Feed models using valid data
+ 5. System delivers Feed of the user
+ 
+ Invalid Data - error course (sad path):
+ 1. System delivers invalid data error
+ 
+ No Connectivity - error course (sad path):
+ 1. System delivers connectivity error
+ 
+*/
+
 import XCTest
 
 final class RemoteFeedLoaderTests: XCTestCase {
