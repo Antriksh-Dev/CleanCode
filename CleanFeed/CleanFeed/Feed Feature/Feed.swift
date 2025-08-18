40b0ca0072e14ad5a9a6bbf77e81379043b82d22
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-18 13:54:13 +0530

    Make 'Feed' model public

--- CleanFeed/CleanFeed/Feed Feature/Feed.swift
+++ CleanFeed/CleanFeed/Feed Feature/Feed.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-struct Feed {
+public struct Feed {
     private let id: UUID
     private let description: String?
     private let location: String?
     private let imageURL: URL
     
-    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
         self.description = description
         self.location = location
