9ef8dbaf1b506b9ccfa479d94ebf2d9ce5253d72
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-18 15:28:55 +0530

    Create RemoteFeedLoaderTests file

---
+++ CleanFeed/CleanFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  CleanFeedTests
+//
+//  Created by Antriksh Verma on 8/18/25.
+//
+
+import XCTest
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+
+}
