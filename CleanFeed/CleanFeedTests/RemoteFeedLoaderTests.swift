b750e4cb29742e5b87b680c42c28cf657b0a5159
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-20 16:07:17 +0530

    Add 'trackForMemoryLeak' helper method

--- CleanFeed/CleanFeedTests/RemoteFeedLoaderTests.swift
+++ CleanFeed/CleanFeedTests/RemoteFeedLoaderTests.swift
@@ -206,6 +206,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
         return (client, sut)
     }
     
+    func trackForMemoryLeak(instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
+        }
+    }
+    
     func anyError() -> Error {
         NSError()
     }
