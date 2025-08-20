1e72b584b4b1c367d51ff9adc180588116352bfb
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-20 19:00:00 +0530

    Add Cache Feed Use Case details

--- CleanFeed/CleanFeedTests/CacheFeedUseCaseTests.swift
+++ CleanFeed/CleanFeedTests/CacheFeedUseCaseTests.swift
@@ -5,31 +5,30 @@
 //  Created by Antriksh Verma on 8/20/25.
 //
 
+/*
+ 
+ Cache Feed Use Case
+ 
+ Data: Feed
+ 
+ Primary course (happy path):
+ 1. Execute "Save Feed" command with above data.
+ 2. System deletes old cache feed.
+ 3. System encodes Feed to be saved.
+ 4. System timestamps the new cache.
+ 5. System saves new cache data.
+ 6. System delivers success message.
+ 
+ Deletion error - error course (sad path):
+ 1. System delivers error.
+ 
+ Saving error - error course (sad path):
+ 1. System delivers error.
+ 
+ */
+
 import XCTest
 
 final class CacheFeedUseCaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
