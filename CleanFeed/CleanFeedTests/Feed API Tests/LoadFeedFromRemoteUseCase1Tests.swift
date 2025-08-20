b9dde3449b454bceea16162b6e41d987881d5b03
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-20 21:51:47 +0530

    Create fileprivate 'Feed' model

--- CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase1Tests.swift
+++ CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase1Tests.swift
@@ -28,6 +28,20 @@
 
 import XCTest
 
+fileprivate struct Feed {
+    private let id: UUID
+    private let description: String?
+    private let location: String?
+    private let imageURL: URL
+    
+    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
 final class LoadFeedFromRemoteUseCase1Tests: XCTestCase {
 
     override func setUpWithError() throws {
