742d0ed9f1d1700689026af49557d983bc0bd82c
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-20 23:00:20 +0530

    Implement 'makeSUT' helper in existing tests

--- CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase1Tests.swift
+++ CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase1Tests.swift
@@ -138,17 +138,14 @@ fileprivate class RemoteFeedLoader: FeedLoader {
 final class LoadFeedFromRemoteUseCase1Tests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (client, _) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (client, sut) = makeSUT(url: url)
         
         sut.load { _ in }
         
@@ -157,8 +154,7 @@ final class LoadFeedFromRemoteUseCase1Tests: XCTestCase {
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (client, sut) = makeSUT(url: url)
         
         sut.load { _ in }
         sut.load { _ in }
