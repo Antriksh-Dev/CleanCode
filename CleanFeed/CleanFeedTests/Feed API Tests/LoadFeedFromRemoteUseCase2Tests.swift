11c0d62a90847992c2b895bd8330fba5d04b1a87
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-22 22:24:19 +0530

    Create 'Feed' model

--- CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
+++ CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
@@ -27,6 +27,21 @@
 
 import XCTest
 
+fileprivate struct Feed {
+    private let id: UUID
+    private let description: String?
+    private let location: String?
+    private let imageURL: URL
+    
+    init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+
 final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
 
 
