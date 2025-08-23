dc7367845af1a4840f2f82534c6b49aa2e3736b7
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-23 19:13:43 +0530

    'RemoteFeedLoader' delivers non empty feed for 200 http response and valid non empty json data

--- CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
+++ CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
@@ -171,6 +171,13 @@ final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
         }
     }
     
+    func test_load_deliversNonEmptyFeedFor200HTTPResponseAndValidJSONData() {
+        let (sut, client) = makeSUT()
+        expect(sut, toCompleteWith: .success(validNonEmptyFeed().models)) {
+            client.complete(withData: validNonEmptyFeed().jsonData, responseStatusCode: 200)
+        }
+    }
+    
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
                          file: StaticString = #file,
@@ -215,19 +222,70 @@ final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
         wait(for: [expectation], timeout: 1.0)
     }
     
-    func anyError() -> Error {
+    private func anyError() -> Error {
         NSError(domain: "Any error", code: 0)
     }
     
-    func invalidJSONData() -> Data {
+    private func invalidJSONData() -> Data {
         "invalid json".data(using: .utf8)!
     }
     
-    func validEmptyJSONData() -> Data {
+    private func validEmptyJSONData() -> Data {
         let jsonString = "{ \"items\" : [] }"
         return jsonString.data(using: .utf8)!
     }
     
+    private func validNonEmptyFeed() -> (models: [Feed], jsonData: Data) {
+        let feedItem1 = Feed(id: UUID(uuidString: "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6")!,
+                             description: "Description 1",
+                             location: "Location 1",
+                             imageURL: URL(string: "https://url-1.com")!)
+        let feedItem2 = Feed(id: UUID(uuidString: "BA298A85-6275-48D3-8315-9C8F7C1CD109")!,
+                             description: nil,
+                             location: "Location 2",
+                             imageURL: URL(string: "https://url-2.com")!)
+        let feedItem3 = Feed(id: UUID(uuidString: "5A0D45B3-8E26-4385-8C5D-213E160A5E3C")!,
+                             description: "Description 3",
+                             location: nil,
+                             imageURL: URL(string: "https://url-3.com")!)
+        let feedItem4 = Feed(id: UUID(uuidString: "FF0ECFE2-2879-403F-8DBE-A83B4010B340")!,
+                             description: nil,
+                             location: nil,
+                             imageURL: URL(string: "https://url-4.com")!)
+        
+        let jsonString = """
+            {
+                "items": [
+                    {
+                        "id": "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+                        "description": "Description 1",
+                        "location": "Location 1",
+                        "image": "https://url-1.com",
+                    },
+                    {
+                        "id": "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                        "location": "Location 2",
+                        "image": "https://url-2.com",
+                    },
+                    {
+                        "id": "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                        "description": "Description 3",
+                        "image": "https://url-3.com",
+                    },
+                    {
+                        "id": "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                        "image": "https://url-4.com",
+                    },
+                ]
+            }
+            """
+        
+        let jsonData = jsonString.data(using: .utf8)!
+        let models = [feedItem1, feedItem2, feedItem3, feedItem4]
+        
+        return (models, jsonData)
+    }
+
     private class HTTPClientSpy: HTTPClient {
         enum ReceivedMessage: Equatable {
             case get(URL)
