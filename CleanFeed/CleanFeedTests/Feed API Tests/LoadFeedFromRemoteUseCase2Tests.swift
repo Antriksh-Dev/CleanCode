166eb4f4b16d45ee5333017a2ee3a88d2eeb3b0b
Antriksh Verma <connecttoantriksh@gmail.com>
2025-08-23 16:37:50 +0530

    Extract sut creation to 'makeSUT' helper method

--- CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
+++ CleanFeed/CleanFeedTests/Feed API Tests/LoadFeedFromRemoteUseCase2Tests.swift
@@ -86,17 +86,14 @@ fileprivate class RemoteFeedLoader {
 final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.receivedMessages.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
         
@@ -105,8 +102,7 @@ final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
         sut.load { _ in }
@@ -115,6 +111,12 @@ final class LoadFeedFromRemoteUseCase2Tests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
     
     private class HTTPClientSpy: HTTPClient {
         enum ReceivedMessage: Equatable {
